bd90b85eff5e2112fc62a098eea7c5f2f56274d5
GKKrumov <geobojur@abv.bg>
2015-11-30 19:15:49 +0200

    Модул 09, Видео 03 - Добавяне на клас за данни

--- Interop/Excel/Program.cs
+++ Interop/Excel/Program.cs
@@ -3,10 +3,20 @@
 namespace Excel
 {
 	class MainClass
-	{
+	{	
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			DataStruct data = new DataStruct ();
+			IOWrite write = new IOWrite (data);
+
+			//Набиране на данни в основната таблица
+			data.addRow ( "Георги", "Крумов", "71" );
+			data.addRow ( "невена", "Крумова", "68");
+
+			//Проверка на таблицата
+			data.printTable ();
+
 		}
 	}
 }
